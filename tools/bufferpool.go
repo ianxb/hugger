1790eb9f4b469ec3cc8809b468c2bf65d0c09bdd
unknown <Bob Xiao>
2018-01-14 21:27:49 +0800

    add pool function

--- tools/bufferpool.go
+++ tools/bufferpool.go
@@ -105,7 +105,7 @@ type myPool struct {
 	bufferNumber    uint32       //current number of buffer
 	total           uint64       //the data number
 	bufChan         chan Buffer  //the channel for buffer
-	closed          int32       //status check variable: atomic operation
+	closed          int32        //status check variable: atomic operation
 	rwlock          sync.RWMutex //lock for current operation
 }
 
@@ -124,45 +124,129 @@ func NewPool(bufferCap uint32, maxBufferNumber uint32) (pool Pool, err error) {
 	return &myPool{bufferCap: bufferCap, maxBufferNumber: maxBufferNumber, bufferNumber: 1, bufChan: bufChan}, nil
 }
 
-func (this *myPool)BufferCap() uint32{
+func (this *myPool) BufferCap() uint32 {
 	return this.bufferCap
 }
-func (this *myPool)MaxBufferNumber() uint32{
+func (this *myPool) MaxBufferNumber() uint32 {
 	return this.maxBufferNumber
 }
-func (this *myPool)BufferNumber() uint32{
+func (this *myPool) BufferNumber() uint32 {
 	return atomic.LoadUint32(&this.bufferNumber)
 }
-func (this *myPool)Total() uint64{
+func (this *myPool) Total() uint64 {
 	return atomic.LoadUint64(&this.total)
 }
-func (this *myPool)Closed() bool{
-	if atomic.LoadInt32(&this.closed) == 0{
+func (this *myPool) Closed() bool {
+	if atomic.LoadInt32(&this.closed) == 0 {
 		return true
 	}
 	return false
 }
+
 var ErrclosedBufferPool = errors.New("closed buffer pool error")
-func (this *myPool)Put(data interface{}) (err error){
+
+func (this *myPool) Put(data interface{}) (err error) {
 	if this.Closed() {
 		return ErrClosedBuffer
 	}
-	var maxCount = this.BufferNumber()*5
+	var maxCount = this.BufferNumber() * 5
 	var count uint32
 	var ok bool
-	for buf := range this.bufChan{
-		ok, err = this.putData(buf,data,&count,maxCount)
-		if ok || err != nil{
+	for buf := range this.bufChan {
+		ok, err = this.putData(buf, data, &count, maxCount)
+		if ok || err != nil {
 			break
 		}
 	}
 	return
 }
 
-func (this *myPool)putData(buf Buffer,data interface{},count *uint32,maxCount uint32)(bool,error){
-if this.Closed(){
-	return false,ErrclosedBufferPool
+func (this *myPool) putData(buf Buffer, data interface{}, count *uint32, maxCount uint32) (ok bool, err error) {
+	if this.Closed() {
+		return false, ErrclosedBufferPool
+	}
+	defer func() {
+		this.rwlock.Lock()
+		if this.Closed() {
+			atomic.AddUint32(&this.bufferNumber, ^uint32(0))
+			err = ErrClosedBuffer
+		} else {
+			this.bufChan <- buf
+		}
+		this.rwlock.Unlock()
+	}()
+
+	ok, err = buf.Put(data)
+	if ok {
+		atomic.AddUint64(&this.total, 1)
+		return
+	}
+	if err != nil {
+		return
+	}
+	(*count)++
+	if *count >= maxCount && this.BufferNumber() < this.MaxBufferNumber() {
+		this.rwlock.Lock()
+		if this.BufferNumber() < this.MaxBufferNumber() {
+			if this.Closed() {
+				this.rwlock.Unlock()
+				return
+			}
+			newBuf, _ := NewBuffer(this.bufferCap)
+			newBuf.Put(data)
+			this.bufChan <- newBuf
+			atomic.AddUint32(&this.bufferNumber, 1)
+			atomic.AddUint64(&this.total, 1)
+			ok = true
+		}
+		this.rwlock.Unlock()
+		*count = 0
+	}
+	return
 }
 
+func (this *myPool) Get() (data interface{}, err error) {
+	if this.Closed() {
+		return nil, ErrclosedBufferPool
+	}
+	var count uint32
+	maxCount := this.BufferNumber() * 10
+	for buf := range this.bufChan {
+		data, err = this.getData(buf, &count, maxCount)
+		if data != nil || err != nil {
+			break
+		}
+	}
+	return
 }
 
+func (this *myPool) getData(buf Buffer, count *uint32, maxCount uint32) (data interface{}, err error) {
+	if this.Closed() {
+		return nil, ErrclosedBufferPool
+	}
+	defer func() {
+		if *count >= maxCount && this.BufferNumber() > 1 {
+			buf.Close()
+			atomic.AddUint32(&this.bufferNumber, ^uint32(0))
+			return
+		}
+		this.rwlock.Lock()
+		if this.Closed() {
+			atomic.AddUint32(&this.bufferNumber, uint32(0))
+			err = ErrclosedBufferPool
+		} else {
+			this.bufChan <- buf
+		}
+		this.rwlock.Unlock()
+	}()
+	data, err = buf.Get()
+	if data != nil {
+		atomic.AddUint64(&this.total, ^uint64(0))
+		return
+	}
+	if err != nil {
+		return
+	}
+	(*count)++
+	return
+}
